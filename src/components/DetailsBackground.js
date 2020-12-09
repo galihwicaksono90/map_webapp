421ba41e00d1b96d62386d9f15929749f98861bc
gorillahobo <galihwicaksono90@gmail.com>
2020-12-09 06:06:16 +0000

    Updates

--- src/components/DetailsBackground.js
+++
@@ -1,6 +0,0 @@
-import "../scss/detailsBackground.scss";
-const DetailsBackground = () => {
-  return <div className="details-background">Hello Worlds</div>;
-};
-
-export default DetailsBackground;
--- src/store/locationStore.js
+++
@@ -1,9 +0,0 @@
-import diengStore from "./diengStore";
-import wadaslintangStore from "./wadaslintangStore";
-
-const locationStore = {
-  dieng: diengStore,
-  wadaslintang: wadaslintangStore,
-};
-
-export default locationStore;
